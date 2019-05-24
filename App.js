779d3e94fe5697923345eb12ce5acec9cf2ac933
Stanisław Chmiela <sjchmiela@gmail.com>
2019-05-24 11:02:42 +0200

    Add nested text to the application

--- App.js
+++ App.js
@@ -1,29 +1,16 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <Text style={styles.outsideText}>
+          Outside text{'\n'}
+          <Text style={styles.insideText}>
+            Inside text — inspect me
+          </Text>
+        </Text>
       </View>
     );
   }
@@ -33,17 +20,17 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  outsideText: {
+    fontSize: 18,
   },
-  instructions: {
+  insideText: {
+    margin: 24,
+    fontSize: 24,
+    fontWeight: 'bold',
     textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
   },
 });
